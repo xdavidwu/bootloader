696fae04daa83417d6023ad14137f301f66287b9
xdavidwu <xdavidwuph@gmail.com>
2022-03-11 00:07:33 +0800

    fix initrd addr

--- arch/aarch64/machines/rpi3.c
+++ arch/aarch64/machines/rpi3.c
@@ -4,5 +4,5 @@
 
 void machine_init() {
 	const struct uart_impl *uart = bcm2835_mini_uart_setup();
-	main(uart, (uint8_t *)0x80000, (uint8_t *)0x800000);
+	main(uart, (uint8_t *)0x80000, (uint8_t *)0x8000000);
 }
